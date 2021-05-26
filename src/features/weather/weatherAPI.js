dde6320a2475956cf8688041e624e92e7c48ad23
James F Ciskanik <ciskanikjf@gmail.com>
2021-05-26 01:15:03 -0400

    refactored weatherAPI to use axios instead of fetch

--- src/features/weather/weatherAPI.js
+++ src/features/weather/weatherAPI.js
@@ -1,13 +1,14 @@
+import axios from "axios";
+
 const API_KEY = "eaf56d2245fe23a69ae8d5033bd236f2";
 const zipCode = "22042";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${API_KEY}`;
 
 export const fetchWeather = async () => {
-  const response = await fetch(API_URL);
-  const json = await response.json();
+  const response = await axios.get(API_URL);
   return {
-    icon: json.weather[0].icon,
-    description: json.weather[0].description,
-    temperature: json.main.temp,
+    icon: response.data.weather[0].icon,
+    description: response.data.weather[0].description,
+    temperature: response.data.main.temp,
   };
 };
